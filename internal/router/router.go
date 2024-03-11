158e7894ca25cd6379a6b2c0d47884dcae163f12
Schalmanoff <yaribakal@gmail.com>
2024-03-11 19:10:46 +0300

    static file server, auth page

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,7 +9,12 @@ import (
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
+	// Auth
 	mux.HandleFunc("/auth", authapi.AuthPage)
+
+	// static
+	staticFileServer := http.FileServer(http.Dir("./web/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", staticFileServer))
 
 	return mux
 }
