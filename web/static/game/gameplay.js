24ce37962eae0c57831c481874a49cc15dcd7a3c
Schalmanoff <yaribakal@gmail.com>
2024-04-02 22:48:33 +0300

    tiny changes

--- web/static/game/gameplay.js
+++ web/static/game/gameplay.js
@@ -1,7 +1,12 @@
 const socket = new WebSocket(`ws://${document.location.host}/ws/gameplay`)
+const canvas = $("#game-canvas")
+let gameID = 0
 
 socket.onopen = (event) => {
-    socket.send("jajaja")
+    canvas.ready(() => {
+        gameID = canvas.data("game-id")
+        socket.send("i am connected to " + gameID)
+    })
 }
 
 socket.onmessage = (event) => {
