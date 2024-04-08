95fde71105adf68e06136d932d7e85679e3dd360
Schalmanoff <yaribakal@gmail.com>
2024-04-08 21:13:26 +0300

    improved image handling

--- internal/server/handler/storage/image.go
+++ internal/server/handler/storage/image.go
@@ -3,6 +3,10 @@ package storage
 import (
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/1001bit/OnlineCanvasGames/internal/server/handler/api"
 )
 
 const imageStoragePath = "./web/image"
@@ -10,17 +14,22 @@ const imageStoragePath = "./web/image"
 var imageFormats = [...]string{"png", "jpg", "jpeg"}
 
 func HandleImage(w http.ResponseWriter, r *http.Request) {
-	fileServer := http.FileServer(http.Dir(imageStoragePath))
+	path := filepath.Clean(r.URL.Path)
+	if strings.Contains(path, "..") {
+		api.ServeJSONMessage("invalid path", http.StatusBadRequest, w)
+		return
+	}
 
 	// check if file.png, file.jpg, etc. exists and use it
 	for _, format := range imageFormats {
-		possiblePath := imageStoragePath + r.URL.Path + "." + format
+		possiblePath := filepath.Join(imageStoragePath, path+"."+format)
 
 		if _, err := os.Stat(possiblePath); err == nil {
-			r.URL.Path += "." + format
-			break
+			http.ServeFile(w, r, possiblePath)
+			return
 		}
 	}
 
+	fileServer := http.FileServer(http.Dir(imageStoragePath))
 	fileServer.ServeHTTP(w, r)
 }
