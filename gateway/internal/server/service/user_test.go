87cf99f12896dcc0bd8a0748c11f559b6032aade
Schalmanoff <yaribakal@gmail.com>
2024-07-31 09:59:08 +0300

    made some tests for api gateway

---
+++ gateway/internal/server/service/user_test.go
@@ -0,0 +1,39 @@
+package service
+
+import "testing"
+
+func TestMapToUser(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    map[string]any
+		expected *User
+	}{
+		{
+			name: "valid",
+			input: map[string]any{
+				"id":   1.0,
+				"name": "bob",
+				"date": "once upon a time",
+			},
+			expected: &User{ID: 1, Name: "bob", Date: "once upon a time"},
+		},
+		{
+			name: "dateless",
+			input: map[string]any{
+				"id":   2.0,
+				"name": "jack",
+			},
+			expected: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		if mapToUser(tc.input) == nil && tc.expected == nil {
+			return
+		}
+
+		if *tc.expected != *mapToUser(tc.input) {
+			t.Error("Error in", tc.name, mapToUser(tc.input), tc.expected)
+		}
+	}
+}
